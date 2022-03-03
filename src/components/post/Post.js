574db178641ba3014679021e7ef826ac6d1c1356
Richa-12y <kricha000@gmail.com>
2022-03-03 22:28:47 +0530

    design

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -18,9 +18,22 @@ export default function Post() {
                         <MoreVert />
                     </div>
                 </div>
-                <div className='postCenter'></div>
-                <div className='postBottom'></div>
+                <div className='postCenter'>
+                    <span className='postText'>How you doing ?</span>
+                    <img className='postImg' src="assets/post/10.jpeg" alt="" />
+                </div>
+                <div className='postBottom'>
+                    <div className='postBottomLeft'>
+                        <img className="likeIcon" src='assets/like.png' alt='' />
+                        <img className="likeIcon" src='assets/heart.png' alt='' />
+                        <span className='postLikeCounter'>64 people liked it</span>
 
+                    </div>
+                    <div className='postBottomRight'>
+                        <span className='postCommentText'>10 comments</span>
+
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/components/rightbar/Rightbar.js
+++ src/components/rightbar/Rightbar.js
@@ -3,6 +3,15 @@ import "./rightbar.css";
 
 export default function Rightbar() {
     return (
-        <div className='rightbar'>Rightbar</div>
+        <div className='rightbar'>
+            <div className='rightbarWrapper'>
+                <div className='birthdayContainer'>
+                    <img className='birthdayImg' src='assets/gift.png' alt='' />
+                    <span className="birthdayText">
+                        <b>Ashu Richu</b> and <b>3 other friends</b> have a birhday today.
+                    </span>
+                </div>
+            </div>
+        </div>
     )
 }
